939fb0a7646aff90184e1a813149c326e44e8ba3
krash76 <krash76@mail.ru>
2022-01-28 18:37:14 +0100

    script.js: fullPrice added to console.log

--- script.js
+++ script.js
@@ -11,9 +11,6 @@ const rollbackSum = fullPrice * (rollback/100);
 
 console.log(typeof title, typeof fullPrice, typeof adaptive);
 console.log(screens.length);
-console.log(screenPrice + " euro");
+console.log("screenPrice: " + screenPrice + " euro", ", fullPrice: " + fullPrice + " euro");
 console.log (screens.toLowerCase().split());
 console.log("Rollback to mediator: " + rollbackSum + " euro");
-
-//alert (title + ": homework Lesson 01");
-//console.log("hw_01 done");
