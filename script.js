cd1d8877ffa166b35575cf696b10d3b301f88a3a
krash76 <krash76@mail.ru>
2022-01-28 12:24:40 +0100

    script.js changed: some values added

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 "use strict";
 
 const title = "JS project";
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive; 
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 100;
+const rollback = 20; //percent
+const fullPrice = 10000;
+const adaptive = true; 
 
-alert (title + ": homework Lesson 01");
-console.log("hw_01 done");
+const rollbackSum = fullPrice * (rollback/100);
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice + " euro");
+console.log (screens.toLowerCase().split());
+console.log("Rollback to mediator: " + rollbackSum + " euro");
+
+//alert (title + ": homework Lesson 01");
+//console.log("hw_01 done");
