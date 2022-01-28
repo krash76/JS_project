3291310e47404d659abd1a63aac27120dc1a6cc2
krash76 <krash76@mail.ru>
2022-01-28 10:20:15 +0100

    code added to script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 "use strict";
 
+const title = "JS project";
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive; 
+
+alert (title + ": homework Lesson 01");
+console.log("hw_01 done");
